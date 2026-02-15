1a4bd1b16266edd7fbec7c35d9017c08f9d1de38
Wil Trahan <twilorip@gmail.com>
2026-02-15 15:39:37 -0500

    name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,8 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <div style={{ color: '#84cc16', fontWeight: 900, letterSpacing: '.02em' }}>
-            WillyT23 Milly Machine
+            DFS_SolverLite
           </div>
-          {/* <div className="chip chip-lime">I love Jaxson Dart</div> */}
         </div>
       </div>
     </div>
